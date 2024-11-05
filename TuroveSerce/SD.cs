a5caaca09b1c753b113fcdfd8874eff561550a4e
OleksanderPylypchuk <o.s.pylypchuk@student.khai.edu>
2024-11-05 10:23:31 +0200

    After Sheet

---
+++ TuroveSerce/SD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuroveSerce.Bot
+{
+	public static class SD
+	{
+		public const string Token = "7936846219:AAGD1rBQoMttPuMQK0ECSF_w6dT06FGAkrk";
+		public const string BotName = "turove_serce_bot";
+		public const long GroupChatId = -4552060748;
+
+		public const string ApplicationName = "Turove Serce";
+		public const string SheetName = "Турове Серце";
+		public const string SheetId = "1w5GaM_NNnDnHIUxFDyxZA5VpNLMG-mztoYWfSF-CnqI";
+
+		public const string ClientId = "1057971373941-35fg8266lu4desbc9ra35mkjh9bdkiue.apps.googleusercontent.com";
+		public const string ClientSecrets = "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";
+	}
+}
